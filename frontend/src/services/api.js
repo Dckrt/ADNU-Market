503e4cf86e87e1d0c10ebeccd34754ebc756f39c
Deckert Dotado <deckertdotado@gmail.com>
2026-04-02 20:28:06 +0800

    connect frontend to backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: 'http://127.0.0.1:5000/api',
+  baseURL: 'https://adnu-market.onrender.com/api',
   headers: {
     'Content-Type': 'application/json'
   }
